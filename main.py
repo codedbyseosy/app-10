f32405b48b45ee86212cc813ce26cbdeb0490d63
eseoseodion <ehodion1@sheffield.ac.uk>
2024-02-16 03:15:28 +0000

    Scraping a webpage

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests # get the webpage's html source code
+import selectorlib # retrieve particular information from that html source code
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+if __name__ == "__main__":
+    print(scrape(URL))
